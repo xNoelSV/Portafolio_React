442042001ee54422df93be6102426e4e18775f74
Noel Sariñena Varela <noelsava25@gmail.com>
2024-02-25 13:48:00 +0100

    Bienvenida

--- portafolio/src/components/Footer.jsx
+++ portafolio/src/components/Footer.jsx
@@ -1,30 +1,59 @@
-import logo from '../../public/instagram.svg'
+import logoInstagram from '../../public/instagram.svg'
+import logoGithub from '../../public/github.svg'
+import logoLinkedin from '../../public/linkedin.svg'
 
 const Footer = () => {
-    const estiloHeader = {
+    const estiloFooter = {
         color: "#607b96"
     }
 
     return (
         <>
-            <footer style={estiloHeader} className="fixed bottom-0 w-full border-t border-[#1E2D3D]">
-                <div
-                    className="w-fit border-r border-[#1E2D3D] p-3 inline-block">
-                    Encuentrame en:
-                </div>
-                <div
-                    className='border-r border-[#1E2D3D] inline-block p-3 w-fit'>
+            <footer style={estiloFooter} className="grid grid-cols-2 fixed bottom-0 w-full border-t border-[#1E2D3D] bg-[#011627]">
+                <div className='inline-flex items-center'>
+                    <p
+                        className="w-fit border-r border-[#1E2D3D] p-3">
+                        Encuentrame en:
+                    </p>
                     <a
-                        href="https://www.instagram.com/_25_noel/">
+                        className='border-r border-[#1E2D3D] p-3 w-fit'
+                        href="https://www.instagram.com/_25_noel/"
+                        rel='noreferrer noopener'
+                        target='_blank'>
                         <img
-                            src={logo}
+                            src={logoInstagram}
                             alt="Logo de Instagram"
-                            width='30'
-                            height='30' />
+                            width='25'
+                            height='25' />
+                    </a>
+                    <a
+                        className='border-r border-[#1E2D3D] p-3 w-fit'
+                        href="https://www.linkedin.com/in/noel-sari%C3%B1ena-varela-5b08bb262/"
+                        rel='noreferrer noopener'
+                        target='_blank'>
+                        <img
+                            src={logoLinkedin}
+                            alt="Logo de Linkedin"
+                            width='25'
+                            height='25' />
                     </a>
                 </div>
-
-            </footer>
+                <div className='flex justify-end'>
+                    <a
+                        className='border-l w-fit border-[#1E2D3D] p-3'
+                        href="https://github.com/xNoelSV"
+                        rel='noreferrer noopener'
+                        target='_blank'>
+                        @xnoelsv
+                        <img
+                            className='inline-block ml-2'
+                            src={logoGithub}
+                            alt="Logo de Github"
+                            width='25'
+                            height='25' />
+                    </a>
+                </div>
+            </footer >
         </>
     )
 }
--- portafolio/src/pages/_bienvenida.jsx
+++ portafolio/src/pages/_bienvenida.jsx
@@ -1,16 +1,37 @@
+import myPhoto from './../images/myPhoto.webp'
+
 const _bienvenida = () => {
 
     return (
         <>
             <div className='w-full'>
                 <div className='grid md:grid-cols-2'>
-                    <div className='h-[90vh] flex justify-end pr-12 items-center'>
-                        <div className='h-50 w-50'>
-                            <h1>Hola</h1>
+                    <div className='h-[90vh] flex justify-end pr-12 items-center md:ml-14'>
+                        <div>
+                            <p>Buenas, soy</p>
+                            <h1 className="text-5xl mt-2">Noel Sariñena</h1>
+                            <h2 className="text-2xl mt-2 text-[#4D5BCE]"> &gt; Full-Stack developer</h2>
+                            <p className="comentario mt-10">// puedes visitar mi perfil de GitHub</p>
+                            <p>
+                                <span className="variable">const </span>
+                                <span className="nombreVariable">githubProfile</span>
+                                <span> = </span>
+                                <a
+                                    className="contenido inline-flex"
+                                    href="https://github.com/xNoelSV"
+                                    rel="noreferrer noopener"
+                                    target="_blank">
+                                    "https://github.com/xNoelSV"
+                                </a>
+                            </p>
                         </div>
                     </div>
-                    <div className='h-[90vh] flex justify-start pl-12 items-center'>
-                        <h1 className=''>Hola</h1>
+                    <div className='h-[90vh] flex justify-center pl-12 items-center'>
+                        <img
+                            src={myPhoto}
+                            alt="Foto de Noel Sariñena"
+                            className='w-fit'
+                        />
                     </div>
                 </div>
             </div>
